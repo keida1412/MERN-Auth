174ea582365e6a9a82a3bb37c235383087e7049d
Srinandan Komanduri <keida1412@gmail.com>
2020-03-27 21:06:53 +0530

    edits

--- index.js
+++ index.js
@@ -29,12 +29,12 @@ app.get("/", (req, res) => {
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`listening on port ${port}`));
