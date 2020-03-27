ec4175ed3dfcc8b8f9c2a8fae781ca9782dae428
Srinandan Komanduri <keida1412@gmail.com>
2020-03-27 22:55:10 +0530

    a

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
   });
 }
 
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.listen(port, () => console.log(__dirname+`listening on port ${port}`));
