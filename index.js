5f4de4482596ff7407ca59a51765b45901347f62
Srinandan Komanduri <keida1412@gmail.com>
2020-03-27 19:57:21 +0530

    Made few changess

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 require("dotenv").config();
 
@@ -24,6 +25,16 @@ app.use("/signup", signup_page);
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
-})
+});
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 app.listen(port, () => console.log(`listening on port ${port}`));
